83594bf9c5cce05db5204508894f1573e9ad07e6
EkZakh <ekzakh14@gmail.com>
2023-01-19 11:34:10 +0300

    prepared choose level fragment layout

---
+++ app/src/main/java/com/ekzak/composition/presentation/ChooseLevelFragment.kt
@@ -0,0 +1,6 @@
+package com.ekzak.composition.presentation
+
+import androidx.fragment.app.Fragment
+import com.ekzak.composition.R
+
+class ChooseLevelFragment : Fragment(R.layout.fragment_choose_level)
