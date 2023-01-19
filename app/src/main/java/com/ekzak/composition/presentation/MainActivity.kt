5fe7955dfaff6b094c9874cd1c2e7976f14876a9
EkZakh <ekzakh14@gmail.com>
2023-01-19 10:29:40 +0300

    prepared welcome screen layout

--- app/src/main/java/com/ekzak/composition/presentation/MainActivity.kt
+++ app/src/main/java/com/ekzak/composition/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ekzak.composition
+package com.ekzak.composition.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ekzak.composition.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
